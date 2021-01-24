42097420a21ca0f92c80dbbacffa8706d4c233ea
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-01-24 20:18:40 +0530

    ts configured

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+// const CracoAlias = require("craco-alias");
+
+// module.exports = {
+//   plugin: CracoAlias,
+//   options: {
+//     source: "options",
+//     baseUrl: "./",
+//     aliases: {
+//       "@app": "./src"
+//     }
+//   }
+// };

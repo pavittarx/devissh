05e5d1aa09266dbd2c028d3ab7835d8e1825c23e
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-01-30 21:37:50 +0530

    project reconfigured

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from 'components/Header';
+import Main from "components/main/index";
+import Footer from 'components/Footer';
 
 import './index.scss';
 
 function App() {
   return <div>
     <Header/>
+    <Main />
     <Footer/>
   </div>;
 }
---
+++ src/components/main/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {ReactComponent as WebDevIllustration} from "assets/illustrations/website_development_illustration.svg";
+
+const Main = () => {
+  return <> <WebDevIllustration /> </>;
+}
+
+export default Main;

283624b707df23a145980abe44cfe9549a5fa6e6
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-01-30 13:25:34 +0530

    node-sass fix

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-import './styles/global.scss';
+import './index.scss';
 
 function App() {
   return <div>

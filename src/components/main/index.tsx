888c9835f4c2c0ba5154af1e2b3768dbe91b4b45
Prabhat Raushan <prabhatraush@gmail.com>
2021-02-11 23:24:25 +0530

    Desktop version interface designed

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import Container from "utils/container";
 import Banner from "./banner";
 import Services from "./services";
-import {ReactComponent as WebDevIllustration} from "assets/illustrations/website_development_illustration.svg";
 
 const Main = () => {
   return <> 
     <Container >
       <Banner/>
     </Container>
-    <Container backcolor="#E5E5E5">
+    <Container backcolor="#192EC90D">
       <Services/>
-    </Container>
-    <WebDevIllustration />   
+    </Container>   
   </>;
 }
 
--- src/components/main/services/index.tsx
+++ src/components/main/services/index.tsx
@@ -17,7 +17,41 @@ function Services(){
                     </div>
                 </div>
             <FlexContainer>
-                <div></div>
+                <div className="building_web">
+                </div>
+                <div className="Quote">
+                    <div className="title">
+                    Web Development 
+                    </div>
+                    <div className="body">
+                    At Devissh, we build websites that are secure, reliable and in-line with your business needs. 
+                    </div>
+                </div>
+            </FlexContainer>
+            <FlexContainer>
+                <div className="Quote">
+                    <div className="title">
+                    App Development
+                    </div>
+                    <div className="body">
+                    In this fast pacing world, if you are looking for an app to meet your specific business needs. We are here for you. 
+                    </div>
+                </div>
+                <div className="mobile_app">
+                </div>
+               
+            </FlexContainer>
+            <FlexContainer>
+                <div className="social_network">
+                </div>
+                <div className="Quote">
+                    <div className="title">
+                    Digital Marketing
+                    </div>
+                    <div className="body">
+                    Building an online presence takes much more than having an app. Every business understand the need to be visible to their potential clients.  
+                    </div>
+                </div>
             </FlexContainer>
         </div>;
 }
